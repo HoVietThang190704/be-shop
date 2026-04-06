f6b2fabeb6a45deb59b4c2c163097d6f4d149d0a
Nguyễn Quang Trung <quangtrungnguyeen@gmail.com>
2026-04-06 17:00:07 +0700

    feat: add comments feature

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use('/api/v1/upload', require('./routes/upload'))
 app.use('/api/v1/messages', require('./routes/messages'))
 app.use('/api/v1/orders', require('./routes/orders'))
 app.use('/api/v1/rewards', require('./routes/rewards'))
+app.use('/api/v1/comments', require('./routes/comments'))
 
 if (process.env.FORCE_GOOGLE_DNS === 'true') {
   dns.setServers(['8.8.8.8', '8.8.4.4']);
